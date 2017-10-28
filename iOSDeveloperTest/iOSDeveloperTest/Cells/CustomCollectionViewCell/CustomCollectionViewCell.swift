5d07c532e62ef1153e4b7c2e0ca5d55d20168e5d
MoeHan <moehan111@gmail.com>
2017-10-28 15:55:15 +0800

    deletion cell finished

--- iOSDeveloperTest/iOSDeveloperTest/Cells/CustomCollectionViewCell/CustomCollectionViewCell.swift
+++ iOSDeveloperTest/iOSDeveloperTest/Cells/CustomCollectionViewCell/CustomCollectionViewCell.swift
@@ -11,15 +11,35 @@ import WebKit
 
 class CustomCollectionViewCell: UICollectionViewCell {
 
-    @IBOutlet weak var webView: WKWebView!
-    
+    @IBOutlet weak var label: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        webView.isOpaque = false
-        let htmlFile = Bundle.main.path(forResource: "RandomChar", ofType: "html")
-        let htmlString = try? String(contentsOfFile: htmlFile!, encoding: String.Encoding.utf8)
-        self.webView.loadHTMLString(htmlString!, baseURL: nil)
-        
+        self.backgroundColor = .random()
+        self.label.text = self.randomizeAvailableLetters()
+        print(self.label.text!)
     }
+    
+    func randomizeAvailableLetters() -> String {
+        let alphabet: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        var availableTiles:String!
+        let rand = Int(arc4random_uniform(26))
+        availableTiles = alphabet[rand]
+        return availableTiles
+    }
+    
+}
 
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(red:   .random(),
+                       green: .random(),
+                       blue:  .random(),
+                       alpha: 1.0)
+    }
 }
--- iOSDeveloperTest/iOSDeveloperTest/ViewController.swift
+++ iOSDeveloperTest/iOSDeveloperTest/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     
     @IBOutlet weak var collectionView: UICollectionView!
     let cellIdentifier:String = "cell"
+    var indexPath:IndexPath!
+    var itemCount = 3
+    var isNewAddedRow:Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setUpNavigationBar()
         
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
@@ -26,6 +30,8 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         self.collectionView.register(UINib(nibName: "CustomCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: cellIdentifier)
         self.collectionView.delegate = self;
         self.collectionView.dataSource = self;
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,7 +41,7 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
 
     // MARK: - UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return self.itemCount
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -43,17 +49,46 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! CustomCollectionViewCell
-        if(indexPath.row == 0){
-            cell.webView.backgroundColor = UIColor.red
+        var cell:CustomCollectionViewCell = CustomCollectionViewCell()
+        if(!self.isNewAddedRow){
+            self.indexPath = indexPath;
+            cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! CustomCollectionViewCell
+        }else{
+            cell = UINib(nibName: "CustomCollectionViewCell", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! CustomCollectionViewCell
         }
-        if(indexPath.row == 1){
-            cell.webView.backgroundColor = UIColor.blue
-        }
-        if(indexPath.row == 2){
-            cell.webView.backgroundColor = UIColor.green
-        }
+        
         return cell
+    }
+    
+    // MARK: - Implementations
+    func setUpNavigationBar() {
+        let homeButton : UIBarButtonItem = UIBarButtonItem(title: "Add", style: UIBarButtonItemStyle.plain, target: self, action:#selector(addCollectionView))
+        
+        let logButton : UIBarButtonItem = UIBarButtonItem(title: "Delete", style: UIBarButtonItemStyle.plain, target: self, action: #selector(deleteCollectionView))
+        
+        self.navigationItem.leftBarButtonItem = homeButton
+        self.navigationItem.rightBarButtonItem = logButton
+    }
+    
+    @objc func deleteCollectionView() {
+        collectionView.performBatchUpdates({ () -> Void in
+            self.collectionView.deleteItems(at: [self.indexPath])
+            if(self.itemCount != 0){
+                self.itemCount -= 1
+            }else{
+                return;
+            }
+            self.collectionView.reloadData()
+        }, completion:nil)
+    }
+    
+    @objc func addCollectionView() {
+//        collectionView.performBatchUpdates({ () -> Void in
+//            self.collectionView.insertItems(at: [self.indexPath])
+//            self.itemCount += 1
+//            self.isNewAddedRow = true
+//            self.collectionView.reloadData()
+//        }, completion:nil)
     }
 }
 
